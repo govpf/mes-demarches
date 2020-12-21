c198a7ded2624c4b959055b358fad013290a5c30
Christian Lautier <15379878+maatinito@users.noreply.github.com>
2020-12-21 11:25:12 -1000

    Added a check to detect when dossier link champ gets float number instead of dossier nb

--- app/models/champs/dossier_link_champ.rb
+++ app/models/champs/dossier_link_champ.rb
@@ -15,4 +15,11 @@
 #  type_de_champ_id :integer
 #
 class Champs::DossierLinkChamp < Champ
+  validate :value, :value_cannot_be_float
+
+  def value_cannot_be_float
+    if value.to_i != value.to_f
+      Raven.capture_message("Numéro de dossier avec décimales #{value}", extra: { debug: true })
+    end
+  end
 end
