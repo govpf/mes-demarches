9addade2d5e0873efb1c5e0a22b47498111bf8ea
maatinito <15379878+maatinito@users.noreply.github.com>
2021-09-01 16:59:09 -1000

    Revert "Swarm: try to include dev tools into ppr"
    This reverts commit eff38eb4865e93180294d2899514cb55d68564b3.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,7 +16,7 @@ Rails.application.configure do
   config.eager_load = true
 
   # Full error reports are disabled and caching is turned on.
-  config.consider_all_requests_local = false
+  config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
@@ -66,8 +66,6 @@ Rails.application.configure do
     logger.formatter = config.log_formatter
     config.logger = ActiveSupport::TaggedLogging.new(logger)
   end
-  config.web_console.development_only = false
-  config.web_console.permissions = '10.0.0.0/16'
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
