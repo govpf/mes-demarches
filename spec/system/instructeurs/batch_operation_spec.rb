2d44bc7662c20b2cc98143262c5107211496aa6b
Christian Lautier <15379878+maatinito@users.noreply.github.com>
2025-03-10 18:39:32 -1000

    ci_tests: fix uploads of screenshots

--- spec/system/instructeurs/batch_operation_spec.rb
+++ spec/system/instructeurs/batch_operation_spec.rb
@@ -8,7 +8,7 @@ describe 'BatchOperation a dossier:', js: true do
 
   context 'with an instructeur' do
     scenario 'create a BatchOperation', chrome: true do
-      puts "LANGUE : #{page.evaluate_script("navigator.language")},\nchemin: #{Selenium::WebDriver::Chrome.path}\nchrome driver: #{Selenium::WebDriver::Service.chrome.path}"
+      puts "LANGUE : #{page.evaluate_script("navigator.language")},\nchemin: #{Selenium::WebDriver::Chrome.path}"
       dossier_1 = create(:dossier, :accepte, procedure: procedure)
       dossier_2 = create(:dossier, :accepte, procedure: procedure)
       dossier_3 = create(:dossier, :accepte, procedure: procedure)
