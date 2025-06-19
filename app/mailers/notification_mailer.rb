5fa7dc190d0764bf39e342d7b26e2a5ddcbd7ed1
maxim.joseau <maxim.joseau@ouidou.fr>
2025-06-19 11:27:06 +0200

    feat: add Lexpol tags for the mail

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -99,6 +99,8 @@ class NotificationMailer < ApplicationMailer
 
   def set_dossier
     @dossier = params[:dossier]
+    LexpolService.inject_mail_tags(@dossier)
+
     configure_defaults_for_user(@dossier.user)
 
     if @dossier.skip_user_notification_email?
