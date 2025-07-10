ecba5797ce8ee638214d86514c7fcb51b2962a0a
maxim.joseau <maxim.joseau@ouidou.fr>
2025-07-10 11:36:23 +0200

    feat: remove legacy nested attributes for champ

--- app/models/champs/repetition_champ.rb
+++ app/models/champs/repetition_champ.rb
@@ -2,8 +2,6 @@
 
 class Champs::RepetitionChamp < Champ
   include ActionView::Helpers::TagHelper
-
-  accepts_nested_attributes_for :champs
   delegate :libelle_for_export, to: :type_de_champ
 
   def rows
