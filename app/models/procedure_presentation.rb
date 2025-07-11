a479c27b6c04b5d7c731d6d17beccfd349c9fcee
maxim.joseau <maxim.joseau@ouidou.fr>
2025-07-11 10:37:58 +0200

    fix: remove duplicate

--- app/models/procedure_presentation.rb
+++ app/models/procedure_presentation.rb
@@ -10,7 +10,6 @@ class ProcedurePresentation < ApplicationRecord
 
   attribute :displayed_columns, :column, array: true
 
-  attribute :displayed_columns, :array, array: true
   attribute :sorted_column, :sorted_column
   def sorted_column = super || procedure.default_sorted_column # Dummy override to set default value
 
