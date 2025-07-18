5311e535d903e0b8344f1202463a4f1b6c7083bb
mfo <mfo@users.noreply.github.com>
2025-07-18 15:20:51 +0200

    refactor(Referentiel.factories): extract from configured trait the referentiel mode which is no more only exact match. move impact to all specs

--- spec/controllers/users/dossiers_controller_spec.rb
+++ spec/controllers/users/dossiers_controller_spec.rb
@@ -2152,7 +2152,7 @@ describe Users::DossiersController, type: :controller do
     end
 
     context 'when champ is pollable' do
-      let(:referentiel) { create(:api_referentiel, :configured) }
+      let(:referentiel) { create(:api_referentiel, :exact_match, :configured) }
       let(:types_de_champ_public) { [{ type: :referentiel, referentiel:, stable_id: }] }
 
       context 'when the requested external_id had not been fetched' do
@@ -2173,7 +2173,7 @@ describe Users::DossiersController, type: :controller do
 
         context 'propagation du prefill (polling)' do
           render_views
-          let(:referentiel) { create(:api_referentiel, :configured) }
+          let(:referentiel) { create(:api_referentiel, :exact_match, :configured) }
           let(:referentiel_stable_id) { 1 }
           let(:types_de_champ_public) do
             [
