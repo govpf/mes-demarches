ce6e09e8d40c77247ed5a206046582b96faebff4
yhru <yhruvok@gmail.com>
2025-08-11 15:07:29 +0200

    fix: use helpers.current_user

--- spec/models/types_de_champ/siret_type_de_champ_spec.rb
+++ spec/models/types_de_champ/siret_type_de_champ_spec.rb
@@ -17,21 +17,6 @@ describe TypesDeChamp::SiretTypeDeChamp do
       expect(columns[0].label).to eq("Identification de l'entreprise – SIRET")
     end
 
-    it "includes required jsonpaths" do
-      expected_paths = [
-        "$.entreprise_raison_sociale",
-        "$.entreprise_siren"
-      ]
-
-      json_columns = columns.filter { _1.is_a?(Columns::JSONPathColumn) }
-      expect(json_columns.map(&:jsonpath)).to include(*expected_paths)
-    end
-
-    it "includes address columns" do
-      address_columns = columns.filter { _1.is_a?(Columns::JSONPathColumn) && _1.jsonpath.match?(/adresse|postal_code/) }
-
-      expect(address_columns).not_to be_empty
-    end
 
     it "does not include jsonpath SIRET column" do
       expect(columns.find { |c| c.is_a?(Columns::JSONPathColumn) && c.jsonpath == "$.siret" }).to be_nil
