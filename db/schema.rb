f9c59ee6cdbd6abbaaa4c2c58e8fe81ec5f08e7c
Christian Lautier <15379878+maatinito@users.noreply.github.com>
2025-08-21 14:27:11 -1000

    fix schema.json & schema.rb & bun.lock
    (cherry picked from commit 25b4e97631df4c336b89bc3b2a07f85c6ed06ed2)

--- db/schema.rb
+++ db/schema.rb
@@ -503,8 +503,8 @@ ActiveRecord::Schema[7.0].define(version: 2025_07_25_181320) do
     t.boolean "for_tiers", default: false, null: false
     t.boolean "forced_groupe_instructeur", default: false, null: false
     t.bigint "groupe_instructeur_id"
-    t.datetime "groupe_instructeur_updated_at", precision: nil
-    t.datetime "hidden_by_administration_at", precision: nil
+    t.datetime "groupe_instructeur_updated_at"
+    t.datetime "hidden_by_administration_at"
     t.datetime "hidden_by_expired_at"
     t.string "hidden_by_reason"
     t.datetime "hidden_by_user_at"
@@ -523,8 +523,8 @@ ActiveRecord::Schema[7.0].define(version: 2025_07_25_181320) do
     t.bigint "parent_dossier_id"
     t.string "prefill_token"
     t.boolean "prefilled"
-    t.string "private_search_terms"
-    t.datetime "processed_at", precision: nil
+    t.text "private_search_terms"
+    t.datetime "processed_at"
     t.bigint "revision_id"
     t.text "search_terms"
     t.string "state"
@@ -533,7 +533,7 @@ ActiveRecord::Schema[7.0].define(version: 2025_07_25_181320) do
     t.datetime "termine_close_to_expiration_notice_sent_at"
     t.datetime "updated_at"
     t.integer "user_id"
-    t.index "to_tsvector('french'::regconfig, (search_terms || (private_search_terms)::text))", name: "index_dossiers_on_search_terms_private_search_terms", using: :gin
+    t.index "to_tsvector('french'::regconfig, (search_terms || private_search_terms))", name: "index_dossiers_on_search_terms_private_search_terms", using: :gin
     t.index "to_tsvector('french'::regconfig, search_terms)", name: "index_dossiers_on_search_terms", using: :gin
     t.index ["archived"], name: "index_dossiers_on_archived"
     t.index ["batch_operation_id"], name: "index_dossiers_on_batch_operation_id"
@@ -991,7 +991,7 @@ ActiveRecord::Schema[7.0].define(version: 2025_07_25_181320) do
     t.string "description"
     t.string "description_pj"
     t.string "description_target_audience"
-    t.datetime "dossiers_count_computed_at", precision: nil
+    t.datetime "dossiers_count_computed_at"
     t.bigint "draft_revision_id"
     t.integer "duree_conservation_dossiers_dans_ds"
     t.boolean "duree_conservation_etendue_par_ds", default: false, null: false
@@ -1029,8 +1029,8 @@ ActiveRecord::Schema[7.0].define(version: 2025_07_25_181320) do
     t.jsonb "sva_svr", default: {}, null: false
     t.text "tags", default: [], array: true
     t.boolean "template", default: false, null: false
-    t.datetime "unpublished_at", precision: nil
-    t.datetime "updated_at", precision: nil, null: false
+    t.datetime "unpublished_at"
+    t.datetime "updated_at", null: false
     t.string "web_hook_url"
     t.datetime "whitelisted_at"
     t.bigint "zone_id"
@@ -1198,7 +1198,7 @@ ActiveRecord::Schema[7.0].define(version: 2025_07_25_181320) do
     t.bigint "dossier_id"
     t.string "instructeur_email"
     t.string "motivation"
-    t.datetime "processed_at", precision: nil
+    t.datetime "processed_at"
     t.string "state"
     t.index ["dossier_id"], name: "index_traitements_on_dossier_id"
   end
