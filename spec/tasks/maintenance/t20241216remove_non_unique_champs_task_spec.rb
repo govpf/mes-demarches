9ecd566f4affb2531ebd16f176f71d37ee65f38b
Christian Lautier <15379878+maatinito@users.noreply.github.com>
2025-09-12 17:41:42 -1000

    fix: stabiliser test flaky T20241216removeNonUniqueChampsTask
    Supprime l'assertion .from() qui dépend de l'ordre d'indexation des champs
    en doublon. En tests parallèles, l'ordre peut varier selon le timing,
    causant des échecs non-déterministes.
    
    Le test vérifie toujours que :
    - Le bon champ final est retourné après nettoyage
    - Le nombre total de champs diminue de 1
    
    # pf: stabilisation pour tests parallèles CI

--- spec/tasks/maintenance/t20241216remove_non_unique_champs_task_spec.rb
+++ spec/tasks/maintenance/t20241216remove_non_unique_champs_task_spec.rb
@@ -15,7 +15,7 @@ module Maintenance
         dossier.champs.create(**type_de_champ.params_for_champ)
       }
 
-      it { expect { subject }.to change { dossier.reload.project_champ(type_de_champ).id }.from(dossier.champs.last.id).to(champ_id) }
+      it { expect { subject }.to change { dossier.reload.project_champ(type_de_champ).id }.to(champ_id) }
       it { expect { subject }.to change { Champ.count }.by(-1) }
     end
   end
