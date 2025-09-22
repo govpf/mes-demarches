281810c3f1724fe00a411f3fd0fb002e0d8453c3
yhru <yhruvok@gmail.com>
2025-09-22 16:32:01 +0200

    fix: remove non metropole spec

--- spec/models/champs/siret_champ_spec.rb
+++ spec/models/champs/siret_champ_spec.rb
@@ -12,35 +12,21 @@ describe Champs::SiretChamp do
   end
 
   describe '#validate' do
-    subject { champ.tap { _1.validate(:champs_public_value) } }
-
     context 'when empty' do
-      it { expect(with_value(nil)).to be_valid }
+      subject { with_value(nil).tap { _1.validate(:champs_public_value) } }
+
+      it { is_expected.to be_valid }
     end
 
-    context 'with invalid format' do
-      before { with_value('12345') }
+    context 'with invalid format (too short)' do
+      subject { with_value('12345').tap { _1.validate(:champs_public_value) } }
 
-      it { subject.errors[:value].should include('doit comporter exactement 14 chiffres') }
+      it { expect(subject.errors[:value]).to include('doit avoir 9 chiffres. Selectionnez un établissement.') }
     end
 
-    context 'with invalid checksum' do
-      before { with_value('12345678901234') }
-
-      it { subject.errors[:value].should include("n‘est pas valide") }
-    end
-
-    context 'with valid format but no etablissement' do
-      before { with_value('12345678901245') }
-
-      it { subject.errors[:value].should include("aucun établissement n’est rattaché à ce numéro") }
-    end
-
+    # PF: Test du cas nominal avec numéro TAHITI valide et établissement lié
     context 'with valid SIRET and etablissement' do
-      before do
-        with_value('12345678901245')
-        champ.etablissement = build(:etablissement, siret: champ.value)
-      end
+      subject { with_value('12345678901245').tap { _1.etablissement = build(:etablissement, siret: _1.value); _1.validate(:champs_public_value) } }
 
       it { is_expected.to be_valid }
     end
