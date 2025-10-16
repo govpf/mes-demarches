5d78dd955e23e9d6c54a3aa3f6cb898c5b27900d
yhru <yhruvok@gmail.com>
2025-10-16 17:00:58 +0200

    fix: resolve schema conflict

--- db/schema.rb
+++ db/schema.rb
@@ -522,7 +522,6 @@ ActiveRecord::Schema[7.0].define(version: 2025_09_30_144334) do
     t.string "mandataire_first_name"
     t.string "mandataire_last_name"
     t.text "motivation"
-    t.datetime "notified_soon_deleted_sent_at", precision: nil
     t.bigint "parent_dossier_id"
     t.string "prefill_token"
     t.boolean "prefilled"
@@ -871,8 +870,6 @@ ActiveRecord::Schema[7.0].define(version: 2025_09_30_144334) do
     t.string "color"
     t.datetime "created_at", null: false
     t.string "name"
-    t.bigint "procedure_id", null: false
-    t.datetime "updated_at", null: false
     t.integer "position"
     t.bigint "procedure_id", null: false
     t.datetime "updated_at", null: false
@@ -1054,6 +1051,7 @@ ActiveRecord::Schema[7.0].define(version: 2025_09_30_144334) do
     t.boolean "procedure_expires_when_termine_enabled", default: true
     t.datetime "published_at"
     t.bigint "published_revision_id"
+    t.boolean "rdv_enabled", default: false, null: false
     t.bigint "replaced_by_procedure_id"
     t.boolean "routing_enabled"
     t.bigint "service_id"
@@ -1090,6 +1088,32 @@ ActiveRecord::Schema[7.0].define(version: 2025_09_30_144334) do
     t.index ["zone_id"], name: "index_procedures_zones_on_zone_id"
   end
 
+  create_table "rdv_connections", force: :cascade do |t|
+    t.string "access_token"
+    t.datetime "created_at", null: false
+    t.datetime "expires_at"
+    t.bigint "instructeur_id", null: false
+    t.string "refresh_token"
+    t.datetime "updated_at", null: false
+    t.index ["instructeur_id"], name: "index_rdv_connections_on_instructeur_id", unique: true
+  end
+
+  create_table "rdvs", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.bigint "dossier_id", null: false
+    t.bigint "instructeur_id", null: false
+    t.string "location_type"
+    t.string "rdv_external_id"
+    t.string "rdv_plan_external_id", null: false
+    t.datetime "starts_at"
+    t.string "status"
+    t.datetime "updated_at", null: false
+    t.index ["dossier_id", "rdv_external_id"], name: "index_rdvs_on_dossier_id_and_rdv_external_id"
+    t.index ["dossier_id", "starts_at"], name: "index_rdvs_on_dossier_id_and_starts_at"
+    t.index ["dossier_id"], name: "index_rdvs_on_dossier_id"
+    t.index ["instructeur_id"], name: "index_rdvs_on_instructeur_id"
+  end
+
   create_table "re_instructed_mails", force: :cascade do |t|
     t.text "body"
     t.datetime "created_at", null: false
@@ -1122,6 +1146,7 @@ ActiveRecord::Schema[7.0].define(version: 2025_09_30_144334) do
     t.string "digest"
     t.string "headers", default: [], array: true
     t.string "hint"
+    t.jsonb "last_response"
     t.string "mode"
     t.string "name", null: false
     t.string "test_data"
@@ -1294,6 +1319,7 @@ ActiveRecord::Schema[7.0].define(version: 2025_09_30_144334) do
     t.datetime "current_sign_in_at"
     t.string "current_sign_in_ip"
     t.string "email", default: "", null: false
+    t.boolean "email_unsubscribed", default: false, null: false
     t.datetime "email_verified_at"
     t.string "encrypted_password", default: "", null: false
     t.integer "failed_attempts", default: 0, null: false
@@ -1419,6 +1445,9 @@ ActiveRecord::Schema[7.0].define(version: 2025_09_30_144334) do
   add_foreign_key "procedures", "procedure_revisions", column: "published_revision_id"
   add_foreign_key "procedures", "services", name: "fk_procedures_services"
   add_foreign_key "procedures", "zones"
+  add_foreign_key "rdv_connections", "instructeurs"
+  add_foreign_key "rdvs", "dossiers"
+  add_foreign_key "rdvs", "instructeurs"
   add_foreign_key "received_mails", "procedures"
   add_foreign_key "referentiel_items", "referentiels"
   add_foreign_key "refused_mails", "procedures"
