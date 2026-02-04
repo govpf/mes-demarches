29f6601875ddead384eeefff7874955360520a0b
max.jos <48556853+yhru@users.noreply.github.com>
2026-02-04 16:10:36 +0100

    fix: add missing helper

--- spec/models/dossier_spec.rb
+++ spec/models/dossier_spec.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 describe Dossier, type: :model do
+  include ActionView::Helpers::OutputSafetyHelper
   include ActionView::Helpers::SanitizeHelper
   include ActionView::Helpers::TextHelper
   include ChampHelper
