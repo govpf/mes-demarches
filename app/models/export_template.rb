demarches-simplifiees/demarches-simplifiees.fr

feat(export): instructeur should be able to share export template with other groups

app/models/export_template.rb
@@ -24,6 +24,8 @@ class ExportTemplate < ApplicationRecord
 
   validates_with ExportTemplateValidator
 
+  scope :sharable, -> { where(shared: true) }
+
   DOSSIER_STATE = Dossier.states.fetch(:en_construction)
 
   # when a pj has been added to a revision, it will not be present in the previous pjs

govpf/mes-demarches

Co-Authored-By: Colin Darie <colin@darie.eu>

app/models/export_template.rb
@@ -24,6 +24,8 @@ class ExportTemplate < ApplicationRecord
 
   validates_with ExportTemplateValidator
 
+  scope :shareable, -> { where(shared: true) }
+
   DOSSIER_STATE = Dossier.states.fetch(:en_construction)
 
   # when a pj has been added to a revision, it will not be present in the previous pjs