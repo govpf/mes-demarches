9f630198d9ccb07210bc85a822f69dc36719fb2c
max.jos <48556853+yhru@users.noreply.github.com>
2026-03-03 11:46:05 +0100

    fix: specific spec

--- app/models/champs/referentiel_champ.rb
+++ app/models/champs/referentiel_champ.rb
@@ -29,7 +29,7 @@ class Champs::ReferentielChamp < Champ
   end
 
   def data=(data)
-    if exact_match? || data.blank?
+    if data.blank? || dossier.nil? || exact_match?
       super(data)
     else
       message_encryptor_service = MessageEncryptorService.new
